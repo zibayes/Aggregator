bc54dc64223f2770ed8dc2e01899cfc69c8a4b8c
Pavel Artemev <sanchello13pv@gmail.com>
2025-01-17 01:17:25 +0700

    fixed some bug in reports processing and added some information retriving from scientific report for register

--- agregator/reports_processing.py
+++ agregator/reports_processing.py
@@ -96,12 +96,11 @@ def extract_text_and_images(current_report, file, progress_recorder, pages_count
     Path(folder).mkdir(exist_ok=True)
 
     # Разделы
-    report_parts = ['АННОТАЦИЯ', 'ОГЛАВЛЕНИЕ']  # , 'ведение', 'список исполнителей работ'
+    report_parts = ['аннотация', 'оглавление']  # , 'ведение', 'список исполнителей работ'
     report_parts_info = {i: '' for i in report_parts}
     table_columns = ['Название отчёта', 'Организация', 'Автор',
-                     'Открытый лист',
-                     'Дата написания',
-                     'Исполнители',
+                     'Открытый лист', 'Населённый пункт', 'Год написания отчёта',
+                     'Вид работ', 'Площадь', 'Исполнители',
                      'Заключение']
     table_columns_info = {i: '' for i in table_columns}
     df = None
@@ -112,6 +111,8 @@ def extract_text_and_images(current_report, file, progress_recorder, pages_count
         current_part = 0
         reading_contents = False
         start_page = 2
+        is_introduction = False
+        introduction_part = None
         time_on_start = datetime.now()
         for page_number in range(len(document)):
             pages_processed = total_processed[0] + page_number
@@ -130,8 +131,8 @@ def extract_text_and_images(current_report, file, progress_recorder, pages_count
             text = page.get_text()
             while True:
                 current_index = re.search(
-                    report_parts[current_part].lower().replace('оглавление', r'(оглавление|содержание)').replace(' ',
-                                                                                                                 r'[ \n]*'),
+                    report_parts[current_part].lower().replace('оглавление', r'оглавление|содержание').replace(' ',
+                                                                                                               r'[ \n]*'),
                     text, re.IGNORECASE)
                 if current_index:
                     current_index = current_index.end()
@@ -141,11 +142,18 @@ def extract_text_and_images(current_report, file, progress_recorder, pages_count
                 next_index = None
                 if current_part + 1 < len(report_parts):
                     next_index = re.search(
-                        report_parts[current_part + 1].replace('оглавление', r'(оглавление|содержание)').replace(' ',
-                                                                                                                 r'[ \n]*')
+                        report_parts[current_part + 1].replace('оглавление', r'оглавление|содержание').replace(' ',
+                                                                                                               r'[ \n]*')
                         .replace('(', r'\(').replace(')', r'\)'), text, re.IGNORECASE)
                     if next_index:
                         reading_contents = False
+
+                if introduction_part != current_part:
+                    is_introduction = False
+                if 'введение' in text.lower() and introduction_part is None:
+                    is_introduction = True
+                    introduction_part = current_part
+
                 text_to_write = text[current_index:next_index.start() if next_index else len(text)]
                 report_parts_info[report_parts[current_part]] += text_to_write
                 text_file.write(
@@ -166,8 +174,6 @@ def extract_text_and_images(current_report, file, progress_recorder, pages_count
                                     row['Страницы'] = row['Страницы'][:row['Страницы'].find('-')]
                             report_parts = list(df['Раздел'])
                             report_parts_info = {i: '' for i in report_parts}
-                            print('BLYAT!1', report_parts)
-                            print('BLYAT!2', report_parts_info)
                             '''
                             report_parts_info = dict(list({i: report_parts_info[i] for i in report_parts if
                                                            i in report_parts_info.keys()}.items()) +
@@ -182,7 +188,7 @@ def extract_text_and_images(current_report, file, progress_recorder, pages_count
                             for title_line in titles:
                                 title = re.search(
                                     r'(^(?!\d+\s*$)\d*\.*.*?(?=…))|(^(?!\d+\s*$)\d+\..*\s.+?(?=…))|^(?!\d+\s*$)\d*\.*.*[^…]*?(?=\n\d+)',
-                                    title_line, re.IGNORECASE)
+                                    title_line, re.MULTILINE)
 
                                 if title:
                                     title = title.group(0).replace('\n', '').replace('  ', ' ').strip()
@@ -199,8 +205,6 @@ def extract_text_and_images(current_report, file, progress_recorder, pages_count
                                 df = df_new
                             report_parts = list(df['Раздел'])
                             report_parts_info = {i: '' for i in report_parts}
-                            print('BLYAT!1', report_parts)
-                            print('BLYAT!2', report_parts_info)
                             current_part = 0
                             report_parts_lower = [s.lower() for s in report_parts]
                             index = -1
@@ -224,7 +228,12 @@ def extract_text_and_images(current_report, file, progress_recorder, pages_count
                         r'н*\s*а*\s*у*\s*ч*\s*н*\s*ы*\s*й*\s*о\s*т\s*ч\s*[её]\s*т[\s\S]*?\d{4}\s*.*г\.*[оду]*', text,
                         re.IGNORECASE)
                     if report_name:
-                        table_columns_info['Название отчёта'] = report_name.group(0)
+                        report_name = report_name.group(0)
+                        table_columns_info['Название отчёта'] = report_name
+
+                        place = re.search(r'г\.[\s\S]+?\d{4}', report_name, re.IGNORECASE)
+                        if place:
+                            table_columns_info['Населённый пункт'] = place.group(0)
 
                     author = re.search(r'выполнил:\s+.*|автор:\s+.*', text, re.IGNORECASE)
                     print('author', author)
@@ -263,6 +272,73 @@ def extract_text_and_images(current_report, file, progress_recorder, pages_count
                         organization = re.search(r'ООО\s+«[^»]+»', text, re.IGNORECASE)
                     if organization:
                         table_columns_info['Организация'] = organization.group(0)
+
+                    year_of_writing = re.search(
+                        r'[А-ЯЁ]{1}[а-яё,]+-*[А-ЯЁ]*[а-яё,]*\s*\d{4}', text)
+                    if year_of_writing:
+                        year_of_writing = year_of_writing.group(0)
+                        if not table_columns_info['Населённый пункт']:
+                            place = re.search(r'[\s\S]*?(?=\d{4})', year_of_writing, re.IGNORECASE)
+                            if place:
+                                table_columns_info['Населённый пункт'] = place.group(
+                                    0).strip() + ' (место написания отчёта)'
+                        year_of_writing = re.search(r'\d{4}', year_of_writing, re.IGNORECASE)
+                        if year_of_writing:
+                            table_columns_info['Год написания отчёта'] = year_of_writing.group(0)
+
+                if 'заключение' in report_parts[current_part].lower() or 'аннотация' in report_parts[
+                    current_part].lower():
+                    if 'аннотация' in report_parts[current_part].lower():
+                        works_type = re.search(
+                            'работы\sпо[\s\S]+?наблюд[^\s]+|работы\sпо[\s\S]+?шур[^\s]+|результ[\s\S]+?раб[^\s]+|обслед[\s\S]+?наслед[^\s]+|археолог[\s\S]+?развед[^\s]+',
+                            text, re.IGNORECASE)
+                        if works_type:
+                            table_columns_info['Вид работ'] = works_type
+                    if not table_columns_info['Площадь']:
+                        square = re.search(
+                            r'Общ[аяей]*\sплощад[\s\S]*?[\d,.]+\s*.*?м2', text, re.IGNORECASE)
+                        if not square:
+                            square = re.search(
+                                r'[Общаяей]*\sплощад[\s\S]*?[\d,.]+\s*.*?м2', text, re.IGNORECASE)
+                        if not square:
+                            square = re.search(
+                                r'[Общаяей]*\sплощад[\s\S]*?[\d,.]+\s*.*?м', text, re.IGNORECASE)
+                        if square:
+                            square = re.search(r'\d+[,.]*\d*\s*.*?м2*', square.group(0), re.IGNORECASE)
+                            if square:
+                                table_columns_info['Площадь'] = square.group(0)
+
+                if is_introduction:
+                    with pdfplumber.open(file) as pdf:
+                        page_tables = pdf.pages[page_number].extract_tables()
+                    if page_tables and len(page_tables[0]) > 0 and len(page_tables[0][0]) == 2:
+                        executors = []
+                        if 'ФИО' in page_tables[0][0] and 'Степень участия' in page_tables[0][0]:
+                            df_new = pd.DataFrame(page_tables[0], columns=['ФИО', 'Степень участия'])
+                            for cell in df_new['ФИО']:
+                                fio = re.search(r'[А-ЯЁ]{1}[а-яё]+\s+[А-ЯЁ]{1}[а-яё]+\s+[А-ЯЁ]{1}[а-яё]+', cell)
+                                if fio:
+                                    executors.append(
+                                        fio.group(0) + ': ' + df_new[df_new['ФИО'] == cell]['Степень участия'])
+                            if executors:
+                                table_columns_info['Исполнители'] = ';\n'.join(executors)
+                        else:
+                            df_new = pd.DataFrame(page_tables[0], columns=['Параметр', 'Значение'])
+                            fio = None
+                            works = None
+                            for index, cell in df_new.iterrows():
+                                if fio is None:
+                                    fio = re.search(r'[А-ЯЁ]{1}[а-яё]+\s+[А-ЯЁ]{1}[а-яё]+\s+[А-ЯЁ]{1}[а-яё]+',
+                                                    cell['Параметр'])
+                                    if fio:
+                                        fio = fio.group(0)
+                                    elif 'Степень участия' in cell['Параметр']:
+                                        works = cell['Значение']
+                                elif 'Степень участия' in cell['Параметр']:
+                                    works = cell['Значение']
+                            if fio and works:
+                                table_columns_info['Исполнители'] = fio + ': ' + works
+
                 if next_index:
                     current_part += 1
                     start_page = page_number + 1
@@ -281,23 +357,26 @@ def extract_text_and_images(current_report, file, progress_recorder, pages_count
         total_processed[0] += len(document)
     document.close()
 
-    current_report.name = table_columns_info['Название отчёта']
-    current_report.organization = table_columns_info['Организация']
-    current_report.author = table_columns_info['Автор']
-    current_report.open_list = table_columns_info[
-        'Открытый лист']  # report_parts_info['ОТКРЫТЫЙ ЛИСТ'] if 'ОТКРЫТЫЙ ЛИСТ' in report_parts_info.keys() else 'Open list info'
-    current_report.writing_date = '2023-10-01'
-    current_report.introduction = report_parts_info[
-        'ВВЕДЕНИЕ'] if 'ВВЕДЕНИЕ' in report_parts_info.keys() else 'Introduction info'
-    current_report.contractors = report_parts_info[
-        'СПИСОК ИСПОЛНИТЕЛЕЙ РАБОТ'] if 'СПИСОК ИСПОЛНИТЕЛЕЙ РАБОТ' in report_parts_info.keys() else 'Contractors info'
-    current_report.place = 'Research place'
-    current_report.area_info = 'Area information'
-    current_report.research_history = 'Research history text'
-    current_report.results = 'Results text'
-    current_report.conclusion = 'Conclusion text'
-    current_report.supplement = supplement_content
-    current_report.content = report_parts_info
+    print('BLYAT!', progress_json['file_groups'][str(report_id)][source_index]['type'])
+    if progress_json['file_groups'][str(report_id)][source_index]['type'] in ('text', 'all'):
+        current_report.name = table_columns_info['Название отчёта']
+        current_report.organization = table_columns_info['Организация']
+        current_report.author = table_columns_info['Автор']
+        current_report.open_list = table_columns_info[
+            'Открытый лист']  # report_parts_info['ОТКРЫТЫЙ ЛИСТ'] if 'ОТКРЫТЫЙ ЛИСТ' in report_parts_info.keys() else 'Open list info'
+        current_report.writing_date = table_columns_info['Год написания отчёта']
+        current_report.introduction = report_parts_info[
+            'ВВЕДЕНИЕ'] if 'ВВЕДЕНИЕ' in report_parts_info.keys() else ''
+        current_report.contractors = report_parts_info[
+            'СПИСОК ИСПОЛНИТЕЛЕЙ РАБОТ'] if 'СПИСОК ИСПОЛНИТЕЛЕЙ РАБОТ' in report_parts_info.keys() else ''
+        current_report.place = table_columns_info['Населённый пункт']
+        current_report.area_info = table_columns_info['Площадь']
+        current_report.research_history = ''
+        current_report.results = ''
+        current_report.conclusion = ''
+        current_report.content = report_parts_info
+    if progress_json['file_groups'][str(report_id)][source_index]['type'] in ('images', 'all'):
+        current_report.supplement = supplement_content
     current_report.save()
 
 
